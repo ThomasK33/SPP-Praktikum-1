f7dd980d4cb8bae15b8301710ec5f2489da1849d
Thomas Kosiewski <thomas.kosiewski@gmx.de>
2016-11-28 20:51:46 +0100

    Initial Commit

---
+++ SPP Praktikum 1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SPP Praktikum 1
+//
+//  Created by Thomas Kosiewski on 28.11.16.
+//  Copyright © 2016 Thomas Kosiewski. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
